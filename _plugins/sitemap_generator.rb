14e43898fb8a077cf192d4234a652ae21ebe8100
Christian Stigen Larsen <csl@sublevel3.org>
2013-12-16 20:43:15 +0100

    Added sitemap generator

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://csl.name'
+  MY_URL = "http://csl.name"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
