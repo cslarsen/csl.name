30f69df27f114601a2cd0fb23f8d9f58ef20f08f
Christian Stigen Larsen <csl@sublevel3.org>
2015-01-17 15:23:18 +0100

    Removes w3c dead links

--- jp2a/gallery/Google.php
+++ jp2a/gallery/Google.php
@@ -57,18 +57,6 @@ the <i>fair use</i> clause, for demonstrational purposes, especially in
 low-resolution, ASCII format.  If I am wrong, I will promptly remove it.
 </div>
 
-<p>
-<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10"
-        alt="Valid XHTML 1.0 Strict" height="31" width="88" style="border: none;"/></a>
-
-<a href="http://jigsaw.w3.org/css-validator/check?uri=referer"><img style="border:0;width:88px;height:31px"
-        src="http://jigsaw.w3.org/css-validator/images/vcss"
-        alt="Valid CSS!" /></a>
-
-<a href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=170873&amp;type=1"
-        width="88" height="31"  alt="SourceForge.net Logo" /></a>
-</p>
-
-</div>  
+</div>
 </body>
 </html>
